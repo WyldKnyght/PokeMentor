417f9ceefb7aadbeb95c82cbcb73ef8e27b783ef
Sourcery AI <>
2023-09-12 23:51:12 +0000

    'Refactored by Sourcery'

--- src/data_collection/card_data/parse_card_text_and fields.py
+++ src/data_collection/card_data/parse_card_text_and fields.py
@@ -3,34 +3,39 @@ from attribute_patterns import patterns  # Import patterns from attribute_patter
 
 # Function to parse card text and extract all fields
 def parse_card_text(card_text):
-    # Initialize parsed_data dictionary
-    parsed_data = {}
-
-    # Use patterns from attribute_patterns.py to extract fields
-    for field, pattern in patterns.items():
-        match = pattern.search(card_text)
-        if match:
-            parsed_data[field] = match.group(1).strip()
-        else:
-            parsed_data[field] = None
-
+    parsed_data = {
+        field: match.group(1).strip()
+        if (match := pattern.search(card_text))
+        else None
+        for field, pattern in patterns.items()
+    }
     # Convert subtypes to a list
     if 'subtypes' in parsed_data and parsed_data['subtypes']:
         parsed_data['subtypes'] = [subtype.strip() for subtype in parsed_data['subtypes'].split(",")]
 
-    # Extract legalities as a dictionary
-    legalities = {}
-    for key in ['unlimited', 'standard', 'expanded']:
-        if key in parsed_data and parsed_data[key]:
-            legalities[key] = parsed_data.pop(key)
+    legalities = {
+        key: parsed_data.pop(key)
+        for key in ['unlimited', 'standard', 'expanded']
+        if key in parsed_data and parsed_data[key]
+    }
     parsed_data['legalities'] = legalities
 
-    # Extract card set data as a dictionary
-    card_set_data = {}
-    for key in ['set_id', 'set_name', 'set_series', 'set_printedTotal', 'set_total', 'set_ptcgoCode',
-                'set_releaseDate', 'set_updatedAt', 'set_symbol', 'set_logo']:
-        if key in parsed_data and parsed_data[key]:
-            card_set_data[key] = parsed_data.pop(key)
+    card_set_data = {
+        key: parsed_data.pop(key)
+        for key in [
+            'set_id',
+            'set_name',
+            'set_series',
+            'set_printedTotal',
+            'set_total',
+            'set_ptcgoCode',
+            'set_releaseDate',
+            'set_updatedAt',
+            'set_symbol',
+            'set_logo',
+        ]
+        if key in parsed_data and parsed_data[key]
+    }
     card_set_data['set_legalities'] = {}
     for key in ['set_standard', 'set_expanded', 'set_unlimited']:
         if key in parsed_data and parsed_data[key]:
